0f9495bc8556cde9664c5bbec21567ed695c6314
febritoriqkhul <febritoriqkhul@gmail.com>
2020-04-16 14:08:44 +0700

    Create file firebase.config.js

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyA-qKkRSEpNBL4WBuiYXnboAFQgGP5iZD0",
+    authDomain: "react-firebase-4cf1b.firebaseapp.com",
+    databaseURL: "https://react-firebase-4cf1b.firebaseio.com",
+    projectId: "react-firebase-4cf1b",
+    storageBucket: "react-firebase-4cf1b.appspot.com",
+    messagingSenderId: "173696264006",
+    appId: "1:173696264006:web:76fc557a5132a73eb9a747"
+};
